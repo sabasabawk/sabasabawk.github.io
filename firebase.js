aab2772fced08d6b126d51b33c441f8e40cc0763
sabasaba-ministry <sabasabawk@gmail.com>
2026-07-06 14:31:40 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCP8y49mI24mkdgpRbhCIm0NzjtUjFGoO8",
+  authDomain: "sabasaba-gospel-ministry.firebaseapp.com",
+  projectId: "sabasaba-gospel-ministry",
+  storageBucket: "sabasaba-gospel-ministry.firebasestorage.app",
+  messagingSenderId: "459706490163",
+  appId: "1:459706490163:web:fa8e1dc6d55c54ed997246"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
