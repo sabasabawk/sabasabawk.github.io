aa963a38063e691379a16ecc8cfcfa38fc0f42fa
sabasaba-ministry <sabasabawk@gmail.com>
2026-07-07 14:09:31 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -9,12 +9,12 @@ onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
 const firebaseConfig = {
-apiKey: "AIzaSyCP8y49mI24mkdgpRbhCIm0NzjtUjFGoO8",
-authDomain: "sabasaba-gospel-ministry.firebaseapp.com",
-projectId: "sabasaba-gospel-ministry",
-storageBucket: "sabasaba-gospel-ministry.firebasestorage.app",
-messagingSenderId: "459706490163",
-appId: "1:459706490163:web:fa8e1dc6d55c54ed997246"
+apiKey: "AIzaSyBy7eWmqoTqnNNGfV2YD9h2rabkCg_-QfU",
+authDomain: "sabasaba-gospel-ministry-414e2.firebaseapp.com",
+projectId: "sabasaba-gospel-ministry-414e2",
+storageBucket: "sabasaba-gospel-ministry-414e2.firebasestorage.app",
+messagingSenderId: "821278457946",
+appId: "1:821278457946:web:3593bd1743970f44c05863"
 };
 
 const app = initializeApp(firebaseConfig);
