6b5dcab0ac956b8554e3cef47b5e3dafec97e0c4
sabasaba-ministry <sabasabawk@gmail.com>
2026-07-10 09:26:39 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,7 +7,12 @@ import {
   signOut,
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
-
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-storage.js";
 import {
   getFirestore,
   collection,
@@ -37,10 +42,15 @@ const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
 export {
   auth,
   db,
+storage,
+ref,
+uploadBytes,
+getDownloadURL,
   collection,
   addDoc,
   getDocs,
