3e582e861d8a532da7119c9b1c46f15fa1bc26f7
sabasaba-ministry <sabasabawk@gmail.com>
2026-07-11 17:22:39 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,74 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+
+import {
+getAuth,
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+signOut,
+onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+import {
+getFirestore,
+collection,
+addDoc,
+getDocs,
+getDoc,
+doc,
+updateDoc,
+deleteDoc,
+increment,
+query,
+orderBy,
+onSnapshot,
+serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+import {
+getStorage,
+ref,
+uploadBytes,
+getDownloadURL,
+deleteObject
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-storage.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyBy7eWmqoTqnNNGfV2YD9h2rabkCg_-QfU",
+authDomain: "sabasaba-gospel-ministry-414e2.firebaseapp.com",
+projectId: "sabasaba-gospel-ministry-414e2",
+storageBucket: "sabasaba-gospel-ministry-414e2.firebasestorage.app",
+messagingSenderId: "821278457946",
+appId: "1:821278457946:web:3593bd1743970f44c05863"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export {
+auth,
+db,
+storage,
+collection,
+addDoc,
+getDocs,
+getDoc,
+doc,
+updateDoc,
+deleteDoc,
+increment,
+query,
+orderBy,
+onSnapshot,
+serverTimestamp,
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+signOut,
+onAuthStateChanged,
+ref,
+uploadBytes,
+getDownloadURL,
+deleteObject
+};
