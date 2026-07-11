86ed442e022b7070504de9b905510ea7b6168a2d
sabasaba-ministry <sabasabawk@gmail.com>
2026-07-11 14:34:42 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -11,7 +11,8 @@ import {
   getStorage,
   ref,
   uploadBytes,
-  getDownloadURL
+  getDownloadURL,
+  deleteObject
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-storage.js";
 import {
   getFirestore,
@@ -33,7 +34,8 @@ const firebaseConfig = {
   apiKey: "AIzaSyBy7eWmqoTqnNNGfV2YD9h2rabkCg_-QfU",
   authDomain: "sabasaba-gospel-ministry-414e2.firebaseapp.com",
   projectId: "sabasaba-gospel-ministry-414e2",
-  storageBucket: "sabasaba-gospel-ministry-414e2.firebasestorage.app",
+  storageBucket:
+"sabasaba-gospel-ministry-414e2.appspot.com"
   messagingSenderId: "821278457946",
   appId: "1:821278457946:web:3593bd1743970f44c05863"
 };
@@ -51,6 +53,7 @@ storage,
 ref,
 uploadBytes,
 getDownloadURL,
+deleteObject,
   collection,
   addDoc,
   getDocs,
