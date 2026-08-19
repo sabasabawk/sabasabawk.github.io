0d8aadd11913ffc6efe5287fb2e68d3cbcb7b7e9
sabasaba-ministry <sabasabawk@gmail.com>
2026-08-19 15:26:39 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -36,9 +36,9 @@ import {
   arrayUnion,
   arrayRemove,
   query,
-  where,
-  orderBy,
-  onSnapshot,
+where,
+orderBy,
+onSnapshot,
   serverTimestamp,
   writeBatch
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
